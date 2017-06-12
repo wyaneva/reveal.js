0f028bd009d2493d8693a6ceab03d093f5d65ef2
Vanya Yaneva <wyaneva@gmail.com>
2017-06-12 13:20:44 +0100

    Changes to code display in the white theme

---
+++ link-revealjs.sh
@@ -0,0 +1,12 @@
+# Execute this file from a directory 
+# in which you want to develop a reveal.js presentation.
+
+ln -s ~/reveal.js/css css
+ln -s ~/reveal.js/lib lib
+ln -s ~/reveal.js/js js
+ln -s ~/reveal.js/plugin plugin
+ln -s ~/reveal.js/package.json package.json
+ln -s ~/reveal.js/Gruntfile.js
+
+npm install
+grunt serve
